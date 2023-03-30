99c4a419d433f4df864ee2a0f993e266a32ecd22
Jialiang Chen <jchen998@wisc.edu>
2023-03-30 17:29:32 +0800

    add two regime estimation

--- codes/tworegime/sarregime_panel_demo.m
+++ codes/tworegime/sarregime_panel_demo.m
@@ -8,30 +8,35 @@
 % j.p.elhorst@rug.nl
 %
 % REFERENCES: 
-% Elhorst J.P., Fréret S. (2009) Evidence of political yardstick competition in France 
+% Elhorst J.P., Frï¿½ret S. (2009) Evidence of political yardstick competition in France 
 % using a two-regime spatial Durbin model with fixed effects. 
 % Journal of Regional Science. Forthcoming.
 
 % Read data
-A=xlsread('simulation_data.xls');
-W=xlsread('weights.xlsx');
+A=xlsread('ct_spdata_long.xlsx');
+W=xlsread('4nnmatrix.xlsx');
 % Row-normalize W
 W=normr(W);
 % Model parameters and y and x variables
-N=300;
-T=20;
+% number of units
+N=283;
+% time periods
+T=9;
 nobs=N*T;
+% how many explanatory variables, exclusing the lagged y (if any), 
+% includinglagged y (if any)
 K=14;
-y=A(:,10); % column number in the data matrix that corresponds to the dependent variable
-dum=A(:,9); % column number in the data matrix that corresponds to the regime indicator
-xh=A(:,[3,4]);% column numbers in the data matrix that correspond to the independent variables, no constant because it will be eliminated
+
+y=A(:,1); % column number in the data matrix that corresponds to the dependent variable
+dum=A(:,32); % column number in the data matrix that corresponds to the regime indicator
+xh=A(:,[11, 12, 13, 17, 20, 21, 22]);% column numbers in the data matrix that correspond to the independent variables, no constant because it will be eliminated
 % Create wx variables
 for t=1:T
     t1=1+(t-1)*N;t2=t*N;
     wx(t1:t2,:)= W*xh(t1:t2,:);
 end
 x=[dum xh wx];
-info.model=1;
+info.model=3;
 results = sarregime_panel(y,x,dum,W,T,info);
-fid = fopen('ols.out','wr');
-prt_spreg(results,[],fid);
+vnames=strvcat('total arrivals','dum','GDPp','salary','population','investment','taxi','hotel','spot5A','lagGDPp','lagslry','lagpopulation','laginvestment','lagtaxi','laghotel','lagspot5A');
+prt_spreg(results,vnames,1);
