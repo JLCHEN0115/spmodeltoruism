7f90d2e4102dfd81d1e6d98c71fd0d0b410129f0
Jialiang Chen <jchen998@wisc.edu>
2023-04-07 15:12:41 +0800

    prepare data for matlab

--- codes/tworegime/sarregime_panel_demo.m
+++ codes/tworegime/sarregime_panel_demo.m
@@ -13,30 +13,34 @@
 % Journal of Regional Science. Forthcoming.
 
 % Read data
-A=xlsread('ct_spdata_long.xlsx');
-W=xlsread('4nnmatrix.xlsx');
+optsData = detectImportOptions('tlaged_logged_ct_spdata_long.xlsx');
+preview('tlaged_logged_ct_spdata_long.xlsx',optsData);
+optsData.SelectedVariableNames = [7:45];
+A = readmatrix('tlaged_logged_ct_spdata_long.xlsx',optsData);
+
+W4nn = readmatrix('4nnmatrix.xlsx','Range','B2:JX284');
 % Row-normalize W
-W=normr(W);
+W4nn=normr(W4nn);
 % Model parameters and y and x variables
 % number of units
 N=283;
 % time periods
-T=9;
+T=8;
 nobs=N*T;
 % how many explanatory variables, exclusing the lagged y (if any), 
-% includinglagged y (if any)
-K=14;
+% including lagged x (if any)
+K=20;
 
 y=A(:,1); % column number in the data matrix that corresponds to the dependent variable
-dum=A(:,32); % column number in the data matrix that corresponds to the regime indicator
-xh=A(:,[11, 12, 13, 17, 20, 21, 22]);% column numbers in the data matrix that correspond to the independent variables, no constant because it will be eliminated
+dum=A(:,36); % column number in the data matrix that corresponds to the regime indicator
+xh=A(:,[11,12,13,16,17,20,21,22,25,26]);% column numbers in the data matrix that correspond to the independent variables, no constant because it will be eliminated
 % Create wx variables
 for t=1:T
     t1=1+(t-1)*N;t2=t*N;
-    wx(t1:t2,:)= W*xh(t1:t2,:);
+    W4nnx(t1:t2,:)= W4nn*xh(t1:t2,:);
 end
-x=[dum xh wx];
+x=[dum xh W4nnx];
 info.model=3;
-results = sarregime_panel(y,x,dum,W,T,info);
-vnames=strvcat('total arrivals','dum','GDPp','salary','population','investment','taxi','hotel','spot5A','lagGDPp','lagslry','lagpopulation','laginvestment','lagtaxi','laghotel','lagspot5A');
+results = sarregime_panel(y,x,dum,W4nn,T,info);
+vnames=char('total arrivals','dum','GDPpc','salary','population','third industry','investment','taxi','hotel','5A spots','green land','average expense','lagGDPpc','lagslry','lagpop','lagteri','laginvest','lagtaxi','laghotel','lagspot5A','laggrnld','lagavexp');
 prt_spreg(results,vnames,1);
