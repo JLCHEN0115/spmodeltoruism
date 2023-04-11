97d4f119f911dce081785a34e3a583631cff369f
Jialiang Chen <jchen998@wisc.edu>
2023-04-11 12:21:13 +0800

    introduce thresholds for GDP per capita

--- codes/tworegime/logged_lagged.m
+++ codes/tworegime/logged_lagged.m
@@ -11,24 +11,25 @@
 % Elhorst J.P., Fr�ret S. (2009) Evidence of political yardstick competition in France 
 % using a two-regime spatial Durbin model with fixed effects. 
 % Journal of Regional Science. Forthcoming.
+clear
 
 % Read data
 optsData = detectImportOptions('tlaged_logged_ct_spdata_long.xlsx');
 preview('tlaged_logged_ct_spdata_long.xlsx',optsData);
-optsData.SelectedVariableNames = [7:45];
+optsData.SelectedVariableNames = [7:53];
 A = readmatrix('tlaged_logged_ct_spdata_long.xlsx',optsData);
 % Read weighting matrices
 % W3nn = readmatrix('3nnmatrix.xlsx','Range','B2:JX284');
 % W4nn = readmatrix('4nnmatrix.xlsx','Range','B2:JX284');
 % W5nn = readmatrix('5nnmatrix.xlsx','Range','B2:JX284');
-% W6nn = readmatrix('6nnmatrix.xlsx','Range','B2:JX284');
+ W6nn = readmatrix('6nnmatrix.xlsx','Range','B2:JX284');
 % Wcont = readmatrix('contmatrix.xlsx','Range','B2:JX284');
 % Wd100nb = readmatrix('d100nbmatrix.xlsx','Range','B2:JX284');
 % Wd150nb = readmatrix('d150nbmatrix.xlsx','Range','B2:JX284');
 % Wd200nb = readmatrix('d200nbmatrix.xlsx','Range','B2:JX284');
 
 % Row-normalize W
-Wd150nb=normr(Wd150nb);
+W6nn=normr(W6nn);
 
 
 % Model parameters and y and x variables
@@ -42,16 +43,16 @@ nobs=N*T;
 K=20;
 
 y=A(:,1); % column number in the data matrix that corresponds to the dependent variable
-dum=A(:,36); % column number in the data matrix that corresponds to the regime indicator
+dum=A(:,40); % column number in the data matrix that corresponds to the regime indicator
 xh=A(:,[11,12,13,16,17,20,21,22,25,26]);% column numbers in the data matrix that correspond to the independent variables, no constant because it will be eliminated
 % Create wx variables
 for t=1:T
     t1=1+(t-1)*N;t2=t*N;
-    Wd150nbx(t1:t2,:)= Wd150nb*xh(t1:t2,:);
+    W6nnx(t1:t2,:)= W6nn*xh(t1:t2,:);
 end
-x=[dum xh Wd150nbx];
+x=[dum xh W6nnx];
 info.model=3;
-results = sarregime_panel(y,x,dum,Wd150nb,T,info);
+results = sarregime_panel(y,x,dum,W6nn,T,info);
 vnames=char('total arrivals','dum','GDPpc','salary','population','third industry','investment','taxi','hotel','5A spots','green land','average expense','lagGDPpc','lagslry','lagpop','lagteri','laginvest','lagtaxi','laghotel','lagspot5A','laggrnld','lagavexp');
 prt_spreg(results,vnames,1);
 
